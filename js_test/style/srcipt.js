f7d84bcc2cc9269b5b544ccedaa47c7a4ef033af
Yevhen <nobi8778@gmail.com>
2021-04-18 16:41:19 +0300

    add js_test

---
+++ js_test/style/srcipt.js
@@ -0,0 +1,28 @@
+"use strict";
+
+let number = 10;
+
+let number2 = 15;
+
+if (number < number2) {
+    console.log('True');
+} else {
+    console.log('False');
+}
+
+let number3 = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+//alert("hi");
+//const result = confirm("Hello my dear");
+//console.log(result);
+//const answer = +prompt("Вам есть 18", "18");
+//console.log(typeof(answer));
+const answers = [];
+//answers[0] = prompt ('Как Ваше имя ?','');
+//answers[1] = prompt ('Как Ваша фамилия ?','');
+//answers[2] = prompt ('Сколько Вам лет ?','');
+
+console.log(typeof(answers));
